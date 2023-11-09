c7503ec3c1e310644381719934b25c0c8adfa704
noahchanoah <apastron33@gmail.com>
2023-11-09 00:05:00 -0500

    Change to numpy

--- sitePercolation.py
+++ sitePercolation.py
@@ -11,16 +11,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-p = 0.1
-
+p = 0.25
+coords = []
 #initalize a square lattice, *heavily* inspiried by Binder
 def generate_lattice(size: int) -> list[list[int]]:
-    lattice = [[0]*size]*size
+    lattice = np.zeros((size,size))
     for i in range(size):
         for j in range(size):
             #draw a random number
-            if(p >= random.uniform(0,1)): #its the same everytime?
+            num = random.uniform(0,1)
+            if(p >= num):
                 lattice[i][j] = 1
+                coords.append((i,j))
             else:
                 lattice[i][j] = 0
     return lattice
@@ -29,7 +31,16 @@ size = 10
 testLattice = generate_lattice(size)
 print(testLattice)
 
-#TODO print out 2d grid in pyplot. Then, DFS to find if any cluster spans the lattice. Then, average Ps for given p. Then, plot Ps vs p and determine pc. Should be smooth!
+
+
+#plt.scatter([x[0] for x in coords], [y[1] for y in coords])
+#plt.show()
+
+
+
+#should i do bond? I guess im feeling lost on what to do. Not in terms of content difficulty but what i should be getting out of each thing?
+#AN UNDIRECTED GRAPH IS A SYMMETRIC MATRIX
+#TODO Then, DFS to find if any cluster spans the lattice. ISSUE: dfs in ternary tree? Then, average Ps for given p. Then, plot Ps vs p and determine pc. Should be smooth!
 
 x = range(size)
 y = range(size)
